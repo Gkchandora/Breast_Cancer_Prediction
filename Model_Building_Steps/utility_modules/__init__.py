7c56b2198b0bb3b3bd7886b15204db1fe5266673
Gkchandora <gkchandora1995@gmail.com>
2021-08-12 17:45:58 +0000

    .

--- Model_Building_Steps/utility_modules/__init__.py
+++ Model_Building_Steps/utility_modules/__init__.py
@@ -1,5 +1,12 @@
 # modules
-__all__ = ["sample"] 
+# __all__ = ["sample"] 
 print("init file invoked")
 def infun(s):
     print("init file executed")
+
+url = "https://raw.githubusercontent.com/Gkchandora/Breast_Cancer_Prediction/main/Model_Building_Steps/utility_modules"
+
+with httpimport.remote_repo(["sample","data_wrangling", "junk"], url):
+    import junk
+    from sample import *
+    from data_wrangling *
