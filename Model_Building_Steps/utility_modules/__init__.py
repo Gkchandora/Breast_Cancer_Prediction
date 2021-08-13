e123c35b849874f76749f156889cd718dd0b93bf
Gkchandora <gkchandora1995@gmail.com>
2021-08-13 06:27:34 +0000

    .

--- Model_Building_Steps/utility_modules/__init__.py
+++ Model_Building_Steps/utility_modules/__init__.py
@@ -3,7 +3,7 @@
 # Alternate approach to get rid from invoking by module.method
 
 # url of the packages
-url = "https://raw.githubusercontent.com/Gkchandora/Breast_Cancer_Prediction/main/Model_Building_Steps"
+url = "https://raw.githubusercontent.com/Gkchandora/Breast_Cancer_Prediction/main/Model_Building_Steps/utility_modules"
 
 # import libraries
 import httpimport 
